f9c49b5fb2698aac90f55a9d3957a4d19cd80ade
Nitin Singh <nitinsinghm111@gmail.com>
2022-06-03 01:56:07 +0530

    Done with PIP

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button')
+
+//select media stream ,pass to video element , then play
+
+async function selectMediaStream() {
+    try {
+
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream
+
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+
+    await videoElement.requestPictureInPicture();
+    button.disabled = false
+})
+
+selectMediaStream();
